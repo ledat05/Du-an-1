d154164eb205c8285cf19fde1619ae89a1542b43
ADMIN <ADMIN@PC228>
2025-05-06 16:51:22 +0700

    Add project files.

---
+++ Du an 1/Controllers/MayTinhController.cs
@@ -0,0 +1,34 @@
+﻿using Du_an_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Du_an_1.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //cách 3: tiếp nhận giá trị tham số thông qua Model
+        public IActionResult XuLy(MayTinhModel mt)
+        {
+            //b1.lấy giá trị các tham số (So1, So2, PhepTinh)
+            //b2.tinh toan ket qua
+            double ketqua = 0;
+            switch (mt.PhepTinh)
+            {
+                case "+": ketqua = mt.So1 + mt.So2; break;
+                case "-": ketqua = mt.So1 - mt.So2; break;
+                case "*": ketqua = mt.So1 * mt.So2; break;
+                case "/": ketqua = mt.So1 / mt.So2; break;
+            }
+            //b3.Tra ket qua ve nguoi dung
+            ViewBag.KQ = ketqua; //truyền dữ liệu cho view thông qua ViewBag
+            return View("Index", mt);
+        }
+    }
+}
---
+++ Du an 1/Models/MayTinhModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Du_an_1.Models
+{
+    public class MayTinhModel
+    {
+        public double So1 { get; set; }
+        public double So2 { get; set; }
+        public string PhepTinh { get; set; }
+    }
+}
